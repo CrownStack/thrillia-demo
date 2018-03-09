05f7258b4424032eba6b60817d25d5479ea9d687
Aashish Dhawan <aashishdhawan@gmail.com>
2018-03-09 09:26:16 +0530

    added search feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public keyUp = new Subject<string>();
+  merchants$: Observable<Merchant[]>
+  selectedMerchants$: Observable<Merchant[]>
+
+  constructor(
+    protected http: Http
+  ) {
+    this.keyUp
+    .map((event: any) => { return event.target.value.toLowerCase() })
+    .debounceTime(50)
+    .distinctUntilChanged()
+    .subscribe(searchString => {
+      console.log(searchString)
+      this.merchants$.subscribe(merchants => {
+        let selectedMerchantsList = merchants.filter(merchant => (merchant.activities.filter(activity => activity.toLowerCase().indexOf(searchString) != -1)).length > 0)
+      })
+    });
+  }
+
+  ngOnInit() {
+    this.merchants$ = this.getAllMerchants()
+  }
+
+  getAllMerchants(params?: any): Observable<Merchant[]> {
+    //The branches list  is going to be constant all the time
+    //therefore loading it from JSON file rather than the API.
+    return this.http.get('assets/merchants.json')
+    .map(res => res.json())
+    .map(res => res.data)   
+  }
+}
+
+
+export interface Merchant {
+  name: string
+  address_city: string
+  address_state: string
+  address_zip: string
+  website: string
+  phone: string
+  activities: string[]
 }
